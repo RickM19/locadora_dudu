29adbc00825d5719498079dd2fe41409564474fd
RickM19 <riquelmo14br@gmail.com>
2025-05-18 16:00:41 -0300

    First commit

---
+++ src/br/edu/ufersa/poo/model/entities/Livro.java
@@ -0,0 +1,15 @@
+package br.edu.ufersa.poo.model.entities;
+
+public class Livro {
+	private int id;
+	private String titulo;
+	private int anoPublicacao;
+	private String genero;
+	private String autor;
+	private int qtdPaginas;
+	private int qtdExemplares;
+	private int qtdDisponivelAluguel;
+	private double valorAluguel;
+	
+	
+}
