0fd89174881156951212e041208037c6a9945f9e
RickM19 <riquelmo14br@gmail.com>
2025-05-18 16:59:35 -0300

    Adicionando a classe de banco simulado

--- src/br/edu/ufersa/poo/model/entities/BancoSimulado.java
+++ src/br/edu/ufersa/poo/model/entities/BancoSimulado.java
@@ -1,5 +1,14 @@
 package br.edu.ufersa.poo.model.entities;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class BancoSimulado {
+	public static List<Livro> livros = new ArrayList<>();
+	public static List<Disco> discos = new ArrayList<>();
+	public static List<Cliente> clientes = new ArrayList<>();
+	public static List<Aluguel> alugueis = new ArrayList<>();
+	public static List<ItemAlugado> itensAlugados = new ArrayList<>();
+	public static List<Usuario> usuarios = new ArrayList<>();
 
 }
