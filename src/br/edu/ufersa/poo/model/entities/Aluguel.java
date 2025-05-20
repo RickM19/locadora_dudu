50bd4d60ecda4e6c55f24536d7e46b03bebe1f72
João Vitor Russo Liberalino <jvrussoliberalino@gmail.com>
2025-05-20 13:33:30 -0300

    Aluguel e item alugado parcialmente encapsulados, sem nenhum método por enquanto.

--- src/br/edu/ufersa/poo/model/entities/Aluguel.java
+++ src/br/edu/ufersa/poo/model/entities/Aluguel.java
@@ -1,5 +1,63 @@
 package br.edu.ufersa.poo.model.entities;
 
 public class Aluguel {
-
+	//Atributos
+	private int idAluguel;
+	private int idCliente;
+	private String dataInicio;
+	private String dataFim;
+	private double valorTotal;
+	private boolean finalizado;
+	
+	//Getters
+	public int getIdAluguel() {
+		return this.idAluguel;
+	}
+	public int getIdCliente() {
+		return this.idCliente;
+	}
+	public String getDataInicio() {
+		return this.dataInicio;
+	}
+	public String getDataFim() {
+		return this.dataFim;
+	}
+	public double getValorTotal() {
+		return this.valorTotal;
+	}
+	public boolean getFinalizado() {
+		return this.finalizado;
+	}
+	
+	//Setters
+	public void setIdAluguel(int idAluguel) {
+		if (idAluguel >= 0) this.idAluguel = idAluguel;
+	}
+	public void setIdCliente(int idCliente) {
+		if (idCliente >= 0) this.idCliente = idCliente;
+	}
+	public void setDataInicio(String dataInicio) {
+		if (dataInicio != null) this.dataInicio = dataInicio;
+	}
+	public void setDataFim(String dataFim) {
+		if (dataFim != null) this.dataFim = dataFim;
+	}
+	public void setValorTotal(double valorTotal) {
+		if (valorTotal >= 0) this.valorTotal = valorTotal;
+	}
+	public void setFinalizado(boolean finalizado) {
+		this.finalizado = finalizado;
+	}
+	
+	//Construtores
+	public Aluguel(int idAluguel, int idCliente, String dataInicio, String dataFim, double valorTotal, boolean finalizado) {
+		setIdAluguel(idAluguel);
+		setIdCliente(idCliente);
+		setDataInicio(dataInicio);
+		setDataFim(dataFim);
+		setValorTotal(valorTotal);
+		setFinalizado(finalizado);
+	}
+	
+	//Métodos
 }
--- src/br/edu/ufersa/poo/model/entities/ItemAlugado.java
+++ src/br/edu/ufersa/poo/model/entities/ItemAlugado.java
@@ -1,5 +1,55 @@
 package br.edu.ufersa.poo.model.entities;
 
 public class ItemAlugado {
-
+	//Atributos
+	private int idAluguel;
+	private int idItem;
+	private boolean tipo;
+	private String nomeItem;
+	private double valorAluguel;
+	
+	//Getters
+	public int getIdAluguel() {
+		return this.idAluguel;
+	}
+	public int getIdItem() {
+		return this.idItem;
+	}
+	public boolean getTipo() {
+		return this.tipo;
+	}
+	public String getNomeItem() {
+		return this.nomeItem;
+	}
+	public double getValorAluguel() {
+		return this.valorAluguel;
+	}
+	
+	//Setters
+	public void setIdAluguel(int idAluguel) {
+		if(idAluguel >= 0) this.idAluguel = idAluguel;
+	}
+	public void setIdItem(int idItem) {
+		if(idItem >= 0) this.idItem = idItem;
+	}
+	public void setTipo(boolean tipo) {
+		this.tipo = tipo;
+	}
+	public void setNomeItem(String nomeItem) {
+		if(nomeItem != null) this.nomeItem = nomeItem;
+	}
+	public void setValorAluguel(double valorAluguel) {
+		if(valorAluguel >= 0) this.valorAluguel = valorAluguel;
+	}
+	
+	//Construtores
+	public ItemAlugado(int idAluguel, int idItem, boolean tipo, String nomeItem, double valorAluguel) {
+		setIdAluguel(idAluguel);
+		setIdItem(idItem);
+		setTipo(tipo);
+		setNomeItem(nomeItem);
+		setValorAluguel(valorAluguel);
+	}
+	
+	//Métodos
 }
